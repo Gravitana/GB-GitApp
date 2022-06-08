239a8d152c983a06854a31093480cfd4797cf204
Vitaliy <v.a.grahov@ya.ru>
2022-06-09 01:16:50 +0500

    add progress bar

--- app/src/main/java/ru/gravitana/gitapp/MainActivity.kt
+++ app/src/main/java/ru/gravitana/gitapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package ru.gravitana.gitapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.gravitana.gitapp.databinding.ActivityMainBinding
 
@@ -16,11 +16,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        showProgress(false)
 
         binding.refreshButton.setOnClickListener {
+            showProgress(true)
             usersRepo.getUsers(
-                onSuccess = adapter::setData,
+                onSuccess = {
+                    showProgress(false)
+                    adapter.setData(it)
+                },
                 onError = {
+                    showProgress(false)
                     Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
                 }
             )
@@ -32,5 +38,10 @@ class MainActivity : AppCompatActivity() {
     private fun initRecyclerView() {
         binding.usersRecyclerview.layoutManager = LinearLayoutManager(this)
         binding.usersRecyclerview.adapter = adapter
+    }
+
+    private fun showProgress(inProgress: Boolean) {
+        binding.progressBar.isVisible = inProgress
+        binding.usersRecyclerview.isVisible = !inProgress
     }
 }
