a29c56bcd34e4829507f5711d200df516dab0e6e
Vitaliy <v.a.grahov@ya.ru>
2022-06-08 22:07:49 +0500

    add base layout

--- app/src/main/java/ru/gravitana/gitapp/MainActivity.kt
+++ app/src/main/java/ru/gravitana/gitapp/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.gravitana.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ru.gravitana.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
