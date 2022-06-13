cd7275ee6ddbfd416ced64a1c452398aad8485ea
Vitaliy <v.a.grahov@ya.ru>
2022-06-13 22:04:28 +0500

    fix: add UserViewHolder to git

---
+++ app/src/main/java/ru/gravitana/gitapp/ui/users/UserViewHolder.kt
@@ -0,0 +1,21 @@
+package ru.gravitana.gitapp.ui.users
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import ru.gravitana.gitapp.R
+import ru.gravitana.gitapp.domain.entities.UserEntity
+import ru.gravitana.gitapp.databinding.ItemUserBinding
+
+class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+) {
+    private val binding = ItemUserBinding.bind(itemView)
+
+    fun bind(userEntity: UserEntity) {
+        binding.userAvatarImageView.load(userEntity.avatarUrl)
+        binding.userLoginTextView.text = userEntity.login
+        binding.userIdTextView.text = userEntity.id.toString()
+    }
+}
